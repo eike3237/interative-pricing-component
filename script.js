62b5347ef54fb011e3fd721ef83d418e0cef6b6d
eike3237 <eike3237@users.noreply.github.com>
2021-02-03 21:32:31 -0300

    slider with color + some emot

---
+++ script.js
@@ -0,0 +1,24 @@
+/*Advantages script */
+var advantages0 = document.querySelector("span.adv0");
+var advantages1 = document.querySelector("span.adv1");
+var advantages2 = document.querySelector("span.adv2");
+
+advantages0.innerText = `✔️ ` + advantages0.innerText;
+advantages1.innerText = `✔️ ` + advantages1.innerText;
+advantages2.innerText = `✔️ ` + advantages2.innerText;
+
+/*Slider */
+var slider = document.getElementById('MyRange');
+var pageview = document.getElementById('pageview');
+
+pageview.innerHTML = (slider.value * 10) + ' K PAGEVIEWS';
+
+slider.oninput = function(){ /*oninput means when some value be selected, something happens in the slider other words*/
+    pageview.innerHTML = (slider.value * 10) + ' K PAGEVIEWS'; /*To keeps uploaded*/
+}
+
+slider.addEventListener("mousemove", function(){
+    var x = slider.value;
+    var colorBg = 'linear-gradient(90deg,  rgb(16, 213, 194) ' + x + '%, rgb(234, 238, 251)' +  x + '%)';
+    slider.style.background = colorBg;
+})
